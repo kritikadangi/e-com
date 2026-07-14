2065015f5974e872c3dabbdde437f5e65ccd6e75
Dipesh <dipesh1dip1@gmail.com>
2026-07-15 00:10:53 +0545

    logout issues fixes

--- logout.php
+++ logout.php
@@ -1,6 +1,20 @@
 <?php
 require_once 'config/database.php';
+
 $role = $_SESSION['role'] ?? null;
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
 session_destroy();
 
 if ($role === 'admin') {
